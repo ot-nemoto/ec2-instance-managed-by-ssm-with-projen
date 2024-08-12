354b2b80b61979de1271b9436faa11f420044a97
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-08-12 05:56:24 +0000

    プライベートEC2を作成するCDK追加

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# replace this
+# ec2-instance-managed-by-ssm-with-projen
+
+- SSM で管理するプライベートネットワークのインスタンスを作成する
+
+## Init project with `projen`
+
+```bash
+npx projen new awscdk-app-ts
+```
+
+## 事前準備
+
+```sh
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=
+```
+
+## ビルド
+
+```sh
+npx projen build
+```
+
+- アプリケーションのビルド
+- CloudFormation テンプレートの生成
+- テストとリンターの実行
+
+## インフラストラクチャの作成
+
+```sh
+npx projen deploy
+```
+
+### インフラストラクチャの削除
+
+```sh
+npx projen destroy
+```
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,57 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    // VPC の作成
+    const vpc = new ec2.Vpc(this, 'main', {
+      maxAzs: 2, // 可用性ゾーンの最大数
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'), // VPC の CIDR ブロック
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'private-subnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+      natGateways: 0, // NAT ゲートウェイの数を 0 に設定
+    });
+    vpc.addInterfaceEndpoint('ssm', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
+    });
+    vpc.addInterfaceEndpoint('ssmmessages', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+    });
+    vpc.addInterfaceEndpoint('ec2messages', {
+      service: ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES,
+    });
+
+    // EC2 インスタンスの作成
+    new ec2.Instance(this, 'instance', {
+      instanceName: 'ec2-instance-managed-by-ssm-with-projen',
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      role: new iam.Role(this, 'role', {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromManagedPolicyArn(
+            this,
+            'AmazonSSMManagedInstanceCore',
+            'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore'
+          ),
+        ],
+      }),
+    });
   }
 }
 
@@ -17,7 +63,9 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'ec2-instance-managed-by-ssm-with-projen-dev', { env: devEnv });
+new MyStack(app, 'ec2-instance-managed-by-ssm-with-projen-dev', {
+  env: devEnv,
+});
 // new MyStack(app, 'ec2-instance-managed-by-ssm-with-projen-prod', { env: prodEnv });
 
 app.synth();
